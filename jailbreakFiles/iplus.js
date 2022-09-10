e3603a577bd497760393088208df802db305acc3
Chomberto <Chomber20@hotmail.com>
2022-09-10 07:10:08 -0500

    legacy/

---
+++ jailbreakFiles/iplus.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "iPlus",
+  alias: "i+",
+  info: {
+    type: "Untethered",
+    firmwares: ["1.1.4"]
+  },
+  compatibility: [
+    {
+      firmwares: [
+        "4A102", // 1.1.4
+      ],
+      devices: [
+        "iPhone1,1", // iPhone
+      ]
+    }, 
+  ]
+}
